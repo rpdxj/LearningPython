be4c62ada61a35a294d6227733a84e6fc6d5c754
rpdxj <41319471+rpdxj@users.noreply.github.com>
2018-07-17 14:57:46 +0800

    Update exer1.py

--- exer1.py
+++ exer1.py
@@ -7,3 +7,5 @@ hundredbday = birthyear + 100
 print ("Hello " + name + " Welcome to Python Programming ")
 
 print("You're going to be 100 years old on the year",hundredbday)
+
+#my first day on python
