b8c166eacb9d40b958a638f6695fadac78f56777
rpdxj <41319471+rpdxj@users.noreply.github.com>
2018-07-17 14:53:15 +0800

    Add files via upload
    Exercise 1 - Character Input

---
+++ exer1.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+currentyr = 2018
+birthyear = currentyr - age
+hundredbday = birthyear + 100
+
+print ("Hello " + name + " Welcome to Python Programming ")
+
+print("You're going to be 100 years old on the year",hundredbday)
